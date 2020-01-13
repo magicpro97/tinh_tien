1b1e2c456c88e26c1954a96338707ff2230ee637
huytri <huytri430@gmail.com>
2020-01-13 23:53:16 +0700

    Complete adding copy activityID feature

--- lib/app/pages/share_page.dart
+++ lib/app/pages/share_page.dart
@@ -1,44 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:tinh_tien/app/data/models/activity/activity.dart';
 import 'package:tinh_tien/app/widgets/app_button.dart';
 import 'package:tinh_tien/common/colors.dart';
 import 'package:tinh_tien/common/dimens.dart';
+import 'package:flash/flash.dart';
 
 class SharePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Activity activity = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Dimens.NORMAL_PADDING,
+      body: Builder(builder: (context) {
+        return SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Dimens.NORMAL_PADDING,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(activity.id),
+                    IconButton(
+                      onPressed: () => _onCopyClick(context, activity),
+                      icon: Icon(
+                        Icons.content_copy,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: Dimens.NORMAL_PADDING,
+                ),
+                QrImage(data: activity.id),
+                SizedBox(
+                  height: Dimens.NORMAL_PADDING,
+                ),
+                AppButton(
+                  text: 'Send a invite link',
+                  onPressed: () {},
+                ),
+                FlatButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Back',
+                      style: Theme.of(context)
+                          .textTheme
+                          .button
+                          .apply(color: AppColors.MAIN_COLOR),
+                    ))
+              ],
+            ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              QrImage(data: 'aaaaaaaa'),
-              SizedBox(
-                height: Dimens.NORMAL_PADDING,
-              ),
-              AppButton(
-                text: 'Send a invite link',
-                onPressed: () {},
-              ),
-              FlatButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text(
-                    'Back',
-                    style: Theme.of(context)
-                        .textTheme
-                        .button
-                        .apply(color: AppColors.MAIN_COLOR),
-                  ))
-            ],
-          ),
-        ),
-      ),
+        );
+      }),
     );
+  }
+
+  void _onCopyClick(BuildContext context, Activity activity) {
+    Clipboard.setData(ClipboardData(text: activity.id))
+        .then((data) {
+      showFlash(
+        context: context,
+        duration: const Duration(seconds: 3),
+        builder: (_, controller) {
+          return Flash.dialog(
+            controller: controller,
+            alignment: const Alignment(0, -0.9),
+            margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+            enableDrag: false,
+            backgroundColor: Colors.black87,
+            child: DefaultTextStyle(
+              style: const TextStyle(fontSize: 16.0, color: Colors.white),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text('Copied'),
+              ),
+            ),
+          );
+        },
+      );
+    });
   }
 }
