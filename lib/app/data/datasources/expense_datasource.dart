6ef7183ea5752f25f72d3c668ba0cca6e2264527
huytri <huytri430@gmail.com>
2020-01-16 21:59:43 +0700

    Update code

---
+++ lib/app/data/datasources/expense_datasource.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:tinh_tien/app/data/models/error/error_response.dart';
+import 'package:tinh_tien/app/data/models/expense/expense.dart';
+import 'package:tinh_tien/app/data/models/people/person.dart';
+import 'package:tinh_tien/app/network/client.dart';
+
+class ExpenseDatasouce {
+  Future<Either<ErrorResponse, Expense>> createExpense(
+      String activityId,
+      List<Person> paidBy,
+      List<Person> participants,
+      double amount,
+      String paidFor) async {
+    try {
+      final response =
+          await dio.post('$ACTIVITIES/$activityId/expenses', data: {
+        "activity": activityId,
+        "amount": amount,
+        "paid_for": paidFor,
+        "people": participants,
+        "paid_by": paidBy
+      });
+      return Right(Expense.fromJson(response.data));
+    } on DioError catch (e) {
+      return Left(ErrorResponse.fromJson(e.response.data));
+    }
+  }
+}
---
+++ lib/app/data/repositories/expense_repository.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:meta/meta.dart';
+import 'package:tinh_tien/app/data/datasources/expense_datasource.dart';
+import 'package:tinh_tien/app/data/models/expense/expense.dart';
+import 'package:tinh_tien/app/data/models/people/person.dart';
+import 'package:tinh_tien/app/network/no_network_connection_exception.dart';
+import 'package:tinh_tien/core/errors/failures/expense_failure.dart';
+
+import 'base_repository.dart';
+
+class ExpenseRepository extends BaseRepository {
+  final ExpenseDatasouce expenseDatasource;
+
+  ExpenseRepository({
+    @required this.expenseDatasource,
+    @required DataConnectionChecker dataConnectionChecker,
+  }) : super(dataConnectionChecker);
+
+  Future<Either<ExpenseFailure, Expense>> createExpense(
+      String activityId,
+      List<Person> paidBy,
+      List<Person> participants,
+      double amount,
+      String paidFor) async {
+    if (await hasNetworkConnection()) {
+      final data = await expenseDatasource.createExpense(
+          activityId, paidBy, participants, amount, paidFor);
+      return data.fold((error) => Left(ExpenseFailure(error.message)),
+          (expense) => Right(expense));
+    } else {
+      throw NoNetworkConnection();
+    }
+  }
+}
