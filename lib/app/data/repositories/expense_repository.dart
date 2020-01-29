7cb1c4a88a431e308fea93ed6fed5c0084488622
magicpro97 <linhnt99x@gmail.com>
2020-01-29 20:03:27 +0700

    Get expenses and expense failure

--- lib/app/data/repositories/expense_repository.dart
+++ lib/app/data/repositories/expense_repository.dart
@@ -1,10 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:tinh_tien/app/data/datasources/expense_datasource.dart';
+import 'package:tinh_tien/app/data/models/expense/expense.dart';
 import 'package:tinh_tien/app/data/models/expense/expense_request.dart';
 import 'package:tinh_tien/app/data/models/no_data.dart';
 import 'package:tinh_tien/app/network/no_network_connection_exception.dart';
-import 'package:tinh_tien/core/errors/failures/activity_failure.dart';
 import 'package:meta/meta.dart';
+import 'package:tinh_tien/core/errors/failures/expense_failure.dart';
 
 class ExpenseRepository {
   final ExpenseDatasource expenseDatasource;
@@ -13,7 +14,7 @@ class ExpenseRepository {
     @required this.expenseDatasource,
   });
 
-  Future<Either<ActivityFailure, NoData>> create({
+  Future<Either<ExpenseFailure, NoData>> create({
     @required String activityId,
     @required ExpenseRequest expenseRequest,
   }) async {
@@ -21,7 +22,7 @@ class ExpenseRepository {
       final data = await expenseDatasource.create(
           activityId: activityId, expenseRequest: expenseRequest);
       return data.fold(
-        (error) => Left(ActivityFailure(error.message)),
+        (error) => Left(ExpenseFailure(error.message)),
         (data) => Right(data),
       );
     } catch (e) {
@@ -29,7 +30,7 @@ class ExpenseRepository {
     }
   }
 
-  Future<Either<ActivityFailure, NoData>> update({
+  Future<Either<ExpenseFailure, NoData>> update({
     @required String activityId,
     @required ExpenseRequest expenseRequest,
     @required String expenseId,
@@ -41,7 +42,7 @@ class ExpenseRepository {
         expenseId: expenseId,
       );
       return data.fold(
-        (error) => Left(ActivityFailure(error.message)),
+        (error) => Left(ExpenseFailure(error.message)),
         (data) => Right(data),
       );
     } catch (e) {
@@ -49,7 +50,7 @@ class ExpenseRepository {
     }
   }
 
-  Future<Either<ActivityFailure, NoData>> delete({
+  Future<Either<ExpenseFailure, NoData>> delete({
     @required String activityId,
     @required String expenseId,
   }) async {
@@ -59,8 +60,22 @@ class ExpenseRepository {
         expenseId: expenseId,
       );
       return data.fold(
-        (error) => Left(ActivityFailure(error.message)),
+        (error) => Left(ExpenseFailure(error.message)),
         (data) => Right(data),
+      );
+    } catch (e) {
+      throw NoNetworkConnection();
+    }
+  }
+
+  Future<Either<ExpenseFailure, List<Expense>>> gets({
+    @required String activityId,
+  }) async {
+    try {
+      final data = await expenseDatasource.gets(activityId: activityId);
+      return data.fold(
+        (error) => Left(ExpenseFailure(error.message)),
+        (expenses) => Right(expenses),
       );
     } catch (e) {
       throw NoNetworkConnection();
